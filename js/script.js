980de655fcea7ba4a3b08d5e2ecadcffc8146875
Trevor <Twomack6823@gmail.com>
2020-11-16 16:18:35 -0800

    Added full menu and injection

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
-let mnuBtn = document.getElementById("menuBtn");
-let injectHere = document.getElementById("injectHere")
+let menuBtn = document.getElementById("menuBtn");
+let injectHere = document.getElementById("injectHere");
+
+
+menuBtn.addEventListener("click", function(){
+    loadData("../pages/menu.html","menuPage");
+});
+
+function loadData(url,dataType){
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if(this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            injectHere.innerHTML = this.responseText;
+          
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
